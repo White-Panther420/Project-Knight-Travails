80a90759c6215860e5237f89cc19e3a01b726746
White_Panther <99096313+White-Panther420@users.noreply.github.com>
2023-10-08 06:48:44 -0700

    Made it to goal square
    The Knight can now move to the target square successfully with BFS and I found a potential solution for constructing a path to find the shortest distance which involves an object. Will figure out how to backtrack to start node from end node.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Creates a new 8x8 matrix to keep track of visited nodes
 const createVistiedAdjMat = () => {
     visitedAdjMat = []
-    for(let i=0; i<8; i++){
+    for(let i=1; i<(9); i++){
         visitedAdjMat[i] = []
-        for(let j=0; j<8; j++){
+        for(let j=1; j<9; j++){
             visitedAdjMat[i][j] = 0
         }
     }
@@ -39,37 +39,74 @@ let xQueue = []
 let yQueue = []
 let counter = 0
 let execution_ounter = 0;
-const dfs = (newVisitedMat, currentSquare) => {
-    let validMoves = []
-    execution_ounter++
-    for(let i=0; i<8; i++){
-        let newMove = arrayArithmetic(currentSquare, moves[i])
-        let y = newMove[0]
-        let x = newMove[1]
-        // Check for valid move and if node has been visited
-        if(y >=0 && y <=7 && x >=0 && x <= 7 && newVisitedMat[y][x] !== 1){
-            newVisitedMat[y][x] = 1
-            counter++
-            validMoves.push(newMove)
+const dfs = (newVisitedMat, currentSquare, targetSquare) => {
+    // Note: Coordinates will be entered in the form [y,x] for newVisitedMat. Everything else will be of the form [x,y]
+    
+    let path = {}
+    while(xQueue.length > 0 && yQueue.length > 0 && (currentSquare[0] !== targetSquare[0] || currentSquare[1] !== targetSquare[1])){
+        let validMoves = []
+        execution_ounter++
+
+        // Make all possible moves
+        for(let i=0; i<8; i++){
+            let newMove = arrayArithmetic(currentSquare, moves[i])
+            let x = newMove[0]
+            let y = newMove[1]
+            // Check for valid move and if node has been visited
+            if(y >=1 && y <=8 && x >=1 && x <= 8 && newVisitedMat[y][x] !== 1){
+                newVisitedMat[y][x] = 1
+                counter++
+                validMoves.push(newMove)
+                xQueue.push(x)
+                yQueue.push(y)
+            }
+        }
+        let xComponent = xQueue.shift()
+        let yComponent = yQueue.shift()
+        path[`Node(${xComponent},${yComponent})`] = validMoves
+
+        console.log(`Current Move: (${xComponent},${yComponent})`)
+        console.log("XQUEUE: " + xQueue)
+        console.log("YQUEUE: " + yQueue)
+        currentSquare[0] = xQueue[0]
+        currentSquare[1] = yQueue[0]
+        console.log("Next Move: " + currentSquare)
+    }
+    console.log("YAY! We found the goal of: " + currentSquare)
+    console.log("PATH")
+    console.log(path)
+    let xFinal = targetSquare[0]
+    let yFinal = targetSquare[1]
+    //TODO: Find a way to see backtrack from final quare to start node
+    // TODO: Find a way to parse the key string to get x,y values
+    for(let key in path){
+        for(let i=0; i<path[key].length; i++){
+            // Searching to see which key the final move coordinates belong to so we can backtrack
+            if(path[key][i][0] === xFinal && path[key][i][1] === yFinal){
+                console.log("FOUND KEY:" + key)
+
+                // Parse Key to get coords
+                // Add coords to new path array
+                // set xF and yF to those parsed coords and repeat loop
+                // End loop once xF and yF are null (we reached start node)
+                // Return new path array
+            }
         }
     }
-    console.log(validMoves)
-    console.log(visitedAdjMat)
-
-    for(let i=0; i<validMoves.length; i++){
-        dfs(newVisitedMat, validMoves[i])
-    }
-    console.log("FINAL")
-    console.log(validMoves)
-    console.log(visitedAdjMat)
-}
+}   
 
 const knightMove = (currentSquare, targetSquare) =>{
     let newVisitedMat = createVistiedAdjMat()
-    const path = dfs(newVisitedMat, currentSquare)
+    // Push first move into queue 
+    xQueue.push(currentSquare[0])
+    yQueue.push(currentSquare[1])
+    // Mark first node as visited
+    newVisitedMat[currentSquare[1]][currentSquare[0]] = 1
+    console.log(newVisitedMat)
+    const path = dfs(newVisitedMat, currentSquare, targetSquare)
 }
 
-knightMove([0,0], [3,3])
+knightMove([1,1], [4,4])
 
 console.log(counter)
 console.log(execution_ounter)
